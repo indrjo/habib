aaa0b727fc3877b1f6b830d8c11eb17390c1101c
fouche <tetrapharmakon@gmail.com>
2020-04-08 12:59:42 +0200

    This is a catchup commit

--- app/Main.hs
+++ app/Main.hs
@@ -6,15 +6,13 @@ import Data.Attoparsec.Text
 import Data.Char
 
 -- import Habib
-entrytype :: Parser String
-entrytype = do
-  entryMarker <- char '@'
-  bibentryType <- many' $ satisfy isAlpha
-  return bibentryType
+articleP :: Parser article
+articleP = do
+  _a
 
 main :: IO ()
-main = do
-  let fileName = "shelah.bib"
-  input <- readFile fileName
-  putStrLn input
-  -- either putStrLn putStrLn $ parseOnly entrytype <$> (head . lines) input
+main = undefined
+  -- let fileName = "shelah.bib"
+  -- input <- readFile fileName
+  -- -- putStrLn input
+  -- either putStrLn putStrLn $ parseOnly entrytype $ (head . lines) input
