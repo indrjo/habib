801b324a78d6912323edbe3712ed0cd884e46ed7
fouche <tetrapharmakon@gmail.com>
2021-12-10 16:20:03 +0200

    bibType and bibKey (stub) parsers

--- src/Habib.hs
+++ src/Habib.hs
@@ -6,6 +6,8 @@ module Habib where
 import qualified Data.Attoparsec.Text as A
 import Data.Char
 import qualified Data.Text as T
+import Control.Applicative
+import Data.Attoparsec (satisfy, notInClass)
 
 data Field
   = Address
@@ -59,10 +61,29 @@ egroup :: A.Parser Char
 egroup = A.char '}'
 
 entryType :: A.Parser T.Text
-entryType = at *> bibType <* bgroup
+entryType = at *> bibType
 
 bibType :: A.Parser T.Text
-bibType = A.takeWhile (/= '{')
+bibType =
+  A.string "article" <|>
+  A.string "booklet" <|> -- booklet must be before book
+  A.string "book" <|>
+  A.string "conference" <|>
+  A.string "inbook" <|>
+  A.string "incollection" <|>
+  A.string "inproceedings" <|>
+  A.string "manual" <|>
+  A.string "mastersthesis" <|>
+  A.string "misc" <|>
+  A.string "phdthesis" <|>
+  A.string "proceedings" <|>
+  A.string "techreport" <|>
+  A.string "unpublished"
+
+bibKey :: A.Parser T.Text
+bibKey = A.takeWhile (A.notInClass "{},~#% \\")
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- https://metacpan.org/dist/Text-BibTeX/view/btparse/doc/bt_language.pod
